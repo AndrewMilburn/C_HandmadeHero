dd1e3bb54169099776cf2dd1766ec089c1e100fc
Andrew Milburn <sidbell.069@gmail.com>
2022-04-24 08:18:28 +0100

    Halfway through Setup #1
    Project set up in VS
    Test script created (write to Msgbox) and ran successfully

---
+++ Handmade_Hero/Handmade_Hero/Test.cpp
@@ -0,0 +1,13 @@
+// test.cpp
+
+#include <windows.h>
+
+int main(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+{
+    return MessageBoxA(NULL, "hello, world", "caption", 0);
+}
+
+void Foo(void)
+{
+
+}
