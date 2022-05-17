0660a8abe5e6dea40e7ecbd7fa0ff07051b4da7f
Andrew Milburn <sidbell.069@gmail.com>
2022-05-17 17:30:53 +0100

    End Day 2 Main Stream
    Displaying a window
    Next session is Q&A for Day 2, then the Q&A gets folded into the Day's stream

--- HandmadeHero/code/win32_handmade.cpp
+++ HandmadeHero/code/win32_handmade.cpp
@@ -1,13 +1,139 @@
+// win32_handmade.cpp
+
 #include <Windows.h>
 
-int CALLBACK
-WinMain(HINSTANCE hInstance,
-        HINSTANCE hPrevInstance,
-        PSTR lpCmdLine,
-        INT nCmdShow)
+LRESULT CALLBACK MainWindowCallback
+(
+    HWND window,
+    UINT message,
+    WPARAM wParam,
+    LPARAM lParam
+)
 {
-    MessageBoxA(0, "This is Handmade Hero", "Handmade",
-        MB_OK | MB_ICONEXCLAMATION);
-    
+    LRESULT result = 0;
+    switch (message)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+            break;
+        }
+
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+            break;
+        }
+
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE\n");
+            break;
+        }
+
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+            break;
+        }   
+
+        case WM_PAINT:
+        {
+            PAINTSTRUCT paint;
+            HDC deviceContext = BeginPaint(window, &paint);
+            
+            int x = paint.rcPaint.left;
+            int y = paint.rcPaint.top;
+            int w = paint.rcPaint.right - paint.rcPaint.left;
+            int h = paint.rcPaint.bottom - paint.rcPaint.top;
+            static DWORD operation = WHITENESS;
+            
+            PatBlt(deviceContext, x, y, w, h, operation);
+            if (operation == WHITENESS)
+            {
+                operation = BLACKNESS;
+            }
+            else
+            {
+                operation = WHITENESS;
+            }
+            
+            EndPaint(window, &paint);
+            break;
+        }
+
+        default :
+        {
+            //OutputDebugStringA("Default\n");
+            result = DefWindowProcA(window, message, wParam, lParam);
+            break;
+        }
+    }
+    return(result);
+}
+
+int CALLBACK
+WinMain
+(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    PSTR lpCmdLine,
+    INT nCmdShow
+)
+
+{
+    WNDCLASS WindowClass = {};
+
+        // TODO: Check if below style flags still relevant
+        WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+        WindowClass.lpfnWndProc = MainWindowCallback;
+        WindowClass.hInstance = hInstance;
+        //WindowClass.hIcon;
+        WindowClass.lpszClassName = "Handmade Hero Window Class";
+        
+        if (RegisterClass(& WindowClass))
+        {
+            HWND windowHandle = CreateWindowEx
+            (
+                0,
+                WindowClass.lpszClassName,
+                "Handmade Hero",
+                WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                CW_USEDEFAULT,
+                0,
+                0,
+                hInstance,
+                0
+            );
+            if (windowHandle)
+            {
+                MSG message;
+                for (;;)
+                {
+                    BOOL messageResult = (GetMessage(&message, 0, 0, 0));
+                    if(messageResult > 0)
+                    {
+                        TranslateMessage(&message);
+                        DispatchMessage(&message);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                // TODO: Log a fail?
+            }
+        }
+        else
+        {
+            // TODO: Log a fail?
+        }
+
     return 0;
 }
