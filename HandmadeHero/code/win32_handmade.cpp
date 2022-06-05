72e4c7bd89b8d50bb417d51d17b7b1f337ffa2ed
Andrew Milburn <sidbell.069@gmail.com>
2022-06-05 06:29:51 +0100

    Day 2 33:03 / 1:00:41
    Balls deep in Window API Functions

--- HandmadeHero/code/win32_handmade.cpp
+++ HandmadeHero/code/win32_handmade.cpp
@@ -2,11 +2,57 @@
 
 #include <windows.h>
 
+LRESULT CALLBACK WindowProc(HWND window, UINT message,
+                            WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugString( "WM_SIZE Detected\n" );
+            break;
+        }
+        case WM_DESTROY:
+        {
+            OutputDebugString( "WM_DESTROY Detected\n" );
+            break;
+        }
+        case WM_CLOSE:
+        {
+            OutputDebugString( "WM_CLOSE Detected\n" );
+            break;
+        }
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugString( "WM_ACTIVATEAPP Detected\n" );
+            break;
+        }
+        default:
+        {
+            //OutputDebugString( "Default Case Detected\n" );
+            break;
+        }
+    }
+
+        
+
+}
+
+
 int  CALLBACK  WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                        LPSTR lpCmdLine, int nShowCmd){
-    
-    MessageBox(0, "Hello Handmade Hero",
-               "HMH #001", MB_OK | MB_ICONINFORMATION);
-    
+                        LPSTR lpCmdLine, int nShowCmd)
+{
+    //MessageBox(0, "Hello Handmade Hero",
+    //           "HMH #001", MB_OK | MB_ICONINFORMATION);
+
+    WNDCLASS WindowClass = {};
+        //TODO: Check whether the below flags are needed
+        WindowClass.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+        WindowClass.lpfnWndProc = ;
+        WindowClass.hInstance = hInstance;
+        //WindowClass.hIcon;
+        WindowClass.lpszClassName="Handmade Hero WindowClass";
+
     return(0);
 }
+
